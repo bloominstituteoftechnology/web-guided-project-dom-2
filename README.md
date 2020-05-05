772b4454aee98b3cbf6e7524ce412568aa124c77
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-05 16:29:01 +0200

    update instructions

---
+++ README.md
@@ -0,0 +1,8 @@
+# DOM II
+
+## Instructions to run this project
+
+- Clone the project from Github
+- Using your command line, navigate into the project folder
+- Run `npm install`. This downloads all dependencies listed in the `package.json` file
+- Run `npm start`. This compiles the project and launches a development server
---
+++ index.js
@@ -0,0 +1,69 @@
+console.log('project is up')
+
+// 👉 TASK 1- Create a function called 'get'
+// that takes a CSS selector as its argument
+// and executes document.querySelector with the argument.
+
+
+// 👉 TASK 2- Using 'get', select the following elements from the DOM:
+
+// BUTTONS
+const launchButton = null
+const confirmButton = null
+const cancelButton = null
+
+// MESSAGES
+const successMessage = null
+const failureMessage = null
+
+// MODAL
+const modal = null
+const modalOpacity = null
+
+
+// 👉 TASK 3- Create a function that launches!
+// It should open the confirmation modal.
+// Add it as an event listener for click events on the launch button.
+
+
+// 👉 TASK 4- Create a function to confirm the launch.
+// It should close the modal and display a success report.
+// Add it as a listener for clicks on the confirmation button.
+
+
+// 👉 TASK 5- Create a function to cancel the launch.
+// It should close the modal and display a failure report.
+// Add it as a listener for clicks on the cancellation button.
+
+
+// 👉 TASK 6- Create a function that closes the modal if
+// the user hits the Escape key on their keyboard.
+// Add it as an event listener for 'keydown' events on document.
+
+
+// 👉 TASK 7- Add to ALL ELEMENTS ON THE PAGE an event listener for click events.
+// It should console.log the target 🎯 of the event.
+// It should also console.log the CURRENT target 🧭 of the event.
+// Play with stopPropagation and stopImmediatePropagation.
+
+
+// 👉 TASK 8- Create a link and prevent its default behavior
+
+
+// 👉 TASK 9- Using the mouseOver event on 'document',
+// log to the console the X and Y coordinates
+// of the mouse pointer, as it moves over the screen.
+
+
+// 👉 TASK 10- [STRETCH] Create helper functions
+function openModal() {
+
+}
+
+function closeModal() {
+
+}
+
+function killReports() {
+
+}
