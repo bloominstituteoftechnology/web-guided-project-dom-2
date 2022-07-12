5e53530676d1faa43e87a1764a09bf3fb0b3aa4f
Casey Harding <casey.r.harding@gmail.com>
2022-07-12 12:55:09 -0400

    Through escape key.

--- src/index.js
+++ src/index.js
@@ -27,25 +27,45 @@ launchButton.addEventListener("click", function(event) {
   console.log(event);
 })
 
+// TODO use event.target to target a specific event if enough time!
+
 // 👉 TASK 3- Create a function that launches!
 // It should open the confirmation modal.
 // Add it as an event listener for click events on the launch button.
-
+function launch() {
+  modal.classList.remove("off");
+}
+launchButton.addEventListener("click", launch);
 
 // 👉 TASK 4- Create a function to confirm the launch.
 // It should close the modal and display a success report.
 // Add it as a listener for clicks on the confirmation button.
-
+function confirm() {
+  modal.classList.add("off");
+  successMessage.classList.remove("off");
+  failureMessage.classList.add("off");
+}
+confirmButton.addEventListener("click", confirm);
 
 // 👉 TASK 5- Create a function to cancel the launch.
 // It should close the modal and display a failure report.
 // Add it as a listener for clicks on the cancellation button.
-
+function cancel() {
+  modal.classList.add("off");
+  failureMessage.classList.remove("off");
+  successMessage.classList.add("off");
+}
+cancelButton.addEventListener("click", cancel);
 
 // 👉 TASK 6- Create a function that closes the modal if
 // the user hits the Escape key on their keyboard.
 // Add it as an event listener for 'keydown' events on document.
-
+function escKey(event) {
+  if (event.key === "Escape") {
+    modal.classList.add("off");
+  }
+}
+document.addEventListener("keydown", escKey);
 
 // 👉 TASK 7- Add to ALL ELEMENTS ON THE PAGE an event listener for click events.
 // It should console.log the target 🎯 of the event.
